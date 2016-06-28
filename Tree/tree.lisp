d486e78bbf704253b3c74ed8736042d0fc079e4f
honix <ted888@ya.ru>
2016-06-28 23:14:09 +0300

    Tree update

--- Tree/tree.lisp
+++ Tree/tree.lisp
@@ -2,10 +2,13 @@
 
 (in-package :sketch)
 
+(declaim (optimize (speed 3)))
+
 (defparameter mouse-x 0)
 (defparameter mouse-y 0)
 
 (defun draw-tree (from power angle split)
+  (declare (float angle) (fixnum power split))
   (if (> power 0)
       (destructuring-bind (x y) from
 	(dotimes (brunch split)
@@ -13,18 +16,25 @@
 		 (current (- brunch wide))
 		 (nx (+ x (* (cos angle) power)))
 		 (ny (+ y (* (sin angle) power))))
-	    (line x y nx ny)
+	    (with-pen (make-pen :weight (/ power 7) :stroke +black+)
+	      (line x y nx ny))
 	    (draw-tree `(,nx ,ny)
-		       (- power 10)
+		       (- power 7)
 		       (+ angle (* current mouse-y) mouse-x)
 		       2))))))
 
-(defsketch tree ()
-  (draw-tree '(200 350) 70 (* +pi+ 1.5) 2))
+(defsketch tree ((width 600) (height 600))
+  (background (rgb 0.3 0.5 0.6))
+  (with-pen (make-pen :fill (rgb 0.99 0.9 0.4))
+    (circle 200 300 300))
+  (with-pen (make-pen :fill (gray 0.1))
+    (rect 0 500 600 200))
+  (draw-tree '(300 500) 70 (* +pi+ 1.5) 2))
+
 
 (defmethod sdl2.kit:mousemotion-event ((window tree) ts bm
 				       x y xrel yrel)
-  (setf mouse-x (/ (- x 200) 200))
-  (setf mouse-y (/ y 100)))
+  (setf mouse-x (/ (- x 300) 300)
+	mouse-y (/ y 200)))
 
 (make-instance 'tree)
