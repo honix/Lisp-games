f5fee75b5abee8741c58e324c28ebbfb3c129b6a
honix <ted888@ya.ru>
2016-08-10 23:41:38 +0300

    basic tamplate

---
+++ SDL-template/main.lisp
@@ -0,0 +1,75 @@
+;;
+;; Visual List Editor
+;;
+
+(ql:quickload '(:cl-opengl :sdl2-ttf :sdl2-image))
+
+(defpackage :vle
+  (:use :cl :cl-opengl :sdl2))
+
+(in-package :vle)
+
+(load (merge-pathnames "utils.lisp"))
+
+(defparameter *time* 0.0)
+(defparameter *delta* 0.0)
+(defparameter *screen-width* 800)
+(defparameter *screen-height* 600)
+
+(defun main-screen (delta)
+  "Update and render"
+  (incf *time* delta)
+  (gl:clear :color-buffer-bit)
+  (gl:color 1 1 1)
+  (text "this is simple sdl+opengl template" 0 0.8 0.6 0)
+  (gl:color 0.5 0 0)
+  (quad-shape 0 0 (* *time* 10) (sin *time*))
+  (gl:color 1 1 1)
+  (simple-line 0.0 0.0 0.5 0.5))
+  
+
+(defun main()
+  "Init all stuff and define events"
+  (with-init (:everything)
+    (sdl2-ttf:init)
+    (with-window (window :title "VLE"
+			 :w *screen-width*
+			 :h *screen-height*
+			 :flags '(:shown :resizable :opengl))
+      (with-gl-context (gl-context window)
+	(gl-make-current window gl-context)	
+	(gl:enable :texture-2d)
+	(gl:enable :blend)
+	(gl:blend-func :src-alpha :one-minus-src-alpha)
+	(resize-viewport *screen-width* *screen-height*)
+	(gl:clear-color 0.1 0.1 0.1 1)
+
+	(sdl2:with-event-loop (:method :poll)
+	  (:keydown     (:keysym keysym)
+			(case (scancode-symbol
+			       (scancode-value keysym))
+			  (:scancode-space
+			   (print "space"))))
+	  (:keyup       (:keysym keysym)
+			(case (scancode-symbol
+			       (scancode-value keysym))
+			  (:scancode-escape
+			      (push-event :quit))))
+	  ; (:x x :y y :xrel xrel :yrel yrel :state state)
+	  ;(:mousemotion (:x x :y y))
+	  (:idle        ()
+			(idler window))
+	  (:windowevent (:event event :data1 width :data2 height)
+			(when (=
+			       event
+			       sdl2-ffi:+sdl-windowevent-size-changed+)
+			   (resize-viewport width height)))
+	  (:quit        ()
+			(sdl2-ttf:quit)
+			(clean-text-hash)
+			(clean-texture-hash)
+			t))))))
+
+(main)
+
+    
---
+++ SDL-template/utils.lisp
@@ -0,0 +1,194 @@
+(in-package :vle)
+
+(defun path (file-name)
+  (merge-pathnames (concatenate 'string "media/" file-name)))
+
+;;;
+;;; loading-utils
+;;;
+
+(defstruct text-texture
+  texture width heigth)
+
+(defparameter *text-hash* (make-hash-table :test #'equal))
+
+(defun clean-text-hash ()
+  (maphash (lambda (key value) 
+	     (gl:delete-textures (list (text-texture-texture value))))
+	   *text-hash*)
+  (clrhash *text-hash*))
+
+(defun make-text (string)
+  (or (gethash string *text-hash*)
+      (progn
+	(when (> (hash-table-count *text-hash*) 64)
+	  (clean-text-hash))
+       (let* ((font         (sdl2-ttf:open-font
+			     (path "saxmono.ttf") 30))
+	      (texture      (car (gl:gen-textures 1)))
+	      (surface      (sdl2-ttf:render-utf8-blended
+			     font string 255 255 255 0))
+	      (surface-w    (surface-width surface))
+	      (surface-h    (surface-height surface))
+	      (surface-data (surface-pixels surface)))
+	 (gl:bind-texture  :texture-2d texture)
+	 (gl:tex-parameter :texture-2d :texture-min-filter :linear)
+	 (gl:tex-parameter :texture-2d :texture-mag-filter :linear)
+	 (gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
+			   :rgba :unsigned-byte surface-data)
+	 (free-surface surface)
+	 (sdl2-ttf:close-font font)
+	 (setf (gethash string *text-hash*)
+	       (make-text-texture :texture texture
+				  :width (/ surface-w 30)
+				  :heigth (/ surface-h 30 -1)))))))
+
+
+(defparameter *texture-hash* (make-hash-table :test #'equal))
+
+(defun clean-texture-hash ()
+  (maphash (lambda (key value) 
+	     (gl:delete-textures (list value)))
+	   *texture-hash*)
+  (clrhash *texture-hash*))
+
+(defun load-texture (file-name &optional (format :rgba)
+				         (filter :linear))
+  (or (gethash file-name *texture-hash*)
+      (progn
+	(when (> (hash-table-count *texture-hash*) 64)
+	  (clean-texture-hash))
+       (let* ((texture      (car (gl:gen-textures 1)))
+	      (surface      (sdl2-image:load-image (path file-name)))
+	      (surface-w    (surface-width surface))
+	      (surface-h    (surface-height surface))
+	      (surface-data (surface-pixels surface)))
+	 (gl:bind-texture  :texture-2d texture)
+	 (gl:tex-parameter :texture-2d :texture-min-filter filter)
+	 (gl:tex-parameter :texture-2d :texture-mag-filter filter)
+	 (gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
+			   format ; :bgr - bmp  :rgba - png
+			   :unsigned-byte surface-data)
+	 (free-surface surface)
+	 (setf (gethash file-name *texture-hash*) texture)))))
+
+;;;
+;;; drawing
+;;;
+
+(defmacro define-textured (fname &body body)
+  `(defun ,fname (texture x y rotation
+		  scale-x &optional scale-y)
+     (gl:bind-texture :texture-2d texture)
+     (gl:push-matrix)
+     (gl:translate x y 0)
+     (gl:rotate rotation 0 0 1)
+     (gl:scale scale-x (or scale-y scale-x) 1)
+     ,@body
+     (gl:end)
+     (gl:pop-matrix)))
+
+(defmacro define-shape (fname &body body)
+  `(defun ,fname (x y rotation
+		  scale-x &optional scale-y)
+     (gl:bind-texture :texture-2d 0)
+     (gl:push-matrix)
+     (gl:translate x y 0)
+     (gl:rotate rotation 0 0 1)
+     (gl:scale scale-x (or scale-y scale-x) 1)
+     ,@body
+     (gl:end)
+     (gl:pop-matrix)))
+
+(define-textured quad-textured
+  (gl:begin :triangle-fan)
+  (gl:tex-coord 0.0 0.0) (gl:vertex -0.1 -0.1)
+  (gl:tex-coord 1.0 0.0) (gl:vertex  0.1 -0.1)
+  (gl:tex-coord 1.0 1.0) (gl:vertex  0.1  0.1)
+  (gl:tex-coord 0.0 1.0) (gl:vertex -0.1  0.1))
+
+(define-shape quad-shape
+  (gl:begin :triangle-fan)
+  (gl:tex-coord 0.0 0.0) (gl:vertex -0.1 -0.1)
+  (gl:tex-coord 1.0 0.0) (gl:vertex  0.1 -0.1)
+  (gl:tex-coord 1.0 1.0) (gl:vertex  0.1  0.1)
+  (gl:tex-coord 0.0 1.0) (gl:vertex -0.1  0.1))
+
+(defun simple-line (x1 y1 x2 y2)
+  (gl:bind-texture :texture-2d 0)
+  (gl:begin :lines)
+  (gl:vertex x1 y1)
+  (gl:vertex x2 y2)
+  (gl:end))
+
+(defun text (string x y size rotation)
+  (let ((text-texture (make-text string)))
+    (with-slots (texture width heigth) text-texture
+      (quad-textured texture x y
+		     rotation (* width size) (* heigth size)))))
+
+;;;
+;;; window-utils
+;;;
+
+(defun idler (window)
+  "Every frame routines"
+  (let ((old-time (get-internal-real-time))
+	(errors (nth-value
+		 1
+		 (ignore-errors ; in-game error messager
+		   (main-screen *delta*)))))
+		   
+    (when errors
+      (gl:load-identity)
+      (gl:color 1 1 1 1)
+      (text (make-text
+	     (write-to-string errors))
+	    0 0 0.4 0))
+    
+    (gl:flush)
+    (gl-swap-window window)
+    
+    (setf *delta*
+	  (/ (- (get-internal-real-time)
+		old-time)
+	     internal-time-units-per-second))))
+
+(defun resize-viewport (width height)
+  (gl:viewport 0 0 width height)
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (let ((asp (/ width height)))
+    (gl:frustum (- asp) asp -1 1 1 100))
+  (gl:translate 0 0 -1)
+  (gl:matrix-mode :modelview))
+
+(defun full-screen (window)
+  (multiple-value-bind (some width height)
+      (get-current-display-mode 0)
+    (set-window-size window width height))
+  (set-window-fullscreen
+   window
+   (setf *fullscreen*
+	 (not *fullscreen*)))
+  (if *fullscreen* (hide-cursor) (show-cursor)))
+
+;;;
+;;; exebutable
+;;;
+
+(defun save ()
+  "Make exebutable"
+  #+sbcl(if (string= (software-type) "Linux")
+	    (sb-ext:save-lisp-and-die
+	     (concatenate 'string
+			  "_builds/abra-" (software-type))
+	     :toplevel #'main :executable t :compression 9)
+	    (sb-ext:save-lisp-and-die
+	     (concatenate 'string
+			  "_builds/abra-" (software-type) ".exe")
+	     :toplevel #'main :executable t :application-type :gui))
+  #+ccl(ccl:save-application
+	(concatenate 'string "_builds/abra-ccl-" (software-type))
+	:toplevel-function #'main
+	:prepend-kernel t))
