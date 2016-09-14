188201ad1326e09355a0056b95f4d23e5619e9f2
honix <ted888@ya.ru>
2016-09-14 13:36:02 +0300

    template upd

--- SDL-openGL-template/main.lisp
+++ SDL-openGL-template/main.lisp
@@ -9,7 +9,7 @@
 
 (in-package :template)
 
-(load (merge-pathnames "utils.lisp"))
+(load "utils.lisp")
 
 (defparameter *time* 0.0)
 (defparameter *delta* 0.0)
@@ -21,18 +21,17 @@
   (incf *time* delta)
   (gl:clear :color-buffer-bit)
   (gl:color 1 1 1)
-  (text "this is simple sdl+opengl template" 0 0.8 0.6 0)
+  (text "this is simple sdl+opengl template" 0 0.8 0.05 0)
   (gl:color 0.5 0 0)
-  (quad-shape 0 0 (* *time* 10) (sin *time*))
+  (quad-shape 0 0 (* *time* 10) (sin *time*) (sin *time*))
   (gl:color 1 1 1)
   (simple-line 0.0 0.0 0.5 0.5))
   
-
 (defun main()
   "Init all stuff and define events"
   (with-init (:everything)
     (sdl2-ttf:init)
-    (with-window (window :title "VLE"
+    (with-window (window :title "-.-"
 			 :w *screen-width*
 			 :h *screen-height*
 			 :flags '(:shown :resizable :opengl))
@@ -44,7 +43,7 @@
 	(resize-viewport *screen-width* *screen-height*)
 	(gl:clear-color 0.1 0.1 0.1 1)
 
-	(sdl2:with-event-loop (:method :poll)
+	(with-event-loop (:method :poll)
 	  (:keydown     (:keysym keysym)
 			(case (scancode-symbol
 			       (scancode-value keysym))
@@ -54,22 +53,23 @@
 			(case (scancode-symbol
 			       (scancode-value keysym))
 			  (:scancode-escape
-			      (push-event :quit))))
-	  ; (:x x :y y :xrel xrel :yrel yrel :state state)
-	  ;(:mousemotion (:x x :y y))
+			   (push-event :quit))))
+	  (:mousemotion (:x x :y y :xrel xrel :yrel yrel :state state)
+			t)
 	  (:idle        ()
 			(idler window))
 	  (:windowevent (:event event :data1 width :data2 height)
 			(when (=
 			       event
 			       sdl2-ffi:+sdl-windowevent-size-changed+)
-			   (resize-viewport width height)))
+			  (resize-viewport width height)))
 	  (:quit        ()
 			(sdl2-ttf:quit)
 			(clean-text-hash)
 			(clean-texture-hash)
 			t))))))
 
-(main)
+(sb-thread:make-thread #'main)
+
 
     
--- SDL-openGL-template/utils.lisp
+++ SDL-openGL-template/utils.lisp
@@ -1,7 +1,38 @@
-(in-package :template)
-
 (defun path (file-name)
   (merge-pathnames (concatenate 'string "media/" file-name)))
+
+;;;
+;;; math
+;;;
+
+(defun lerp (from to amount)
+  (* (- from to)
+     amount))
+
+;;;
+;;; color
+;;;
+
+(defun hsv-to-rgb (h s v)
+  (let* ((c (* v s))
+	 (x (* c (- 1 (abs (- (mod (/ h 60) 2) 1)))))
+	 (m (- v c))
+	 (rgb-n (cond ((<= 0   h  60) (list c x 0))
+		      ((<= 60  h 120) (list x c 0))
+		      ((<= 120 h 180) (list 0 c x))
+		      ((<= 180 h 240) (list 0 x c))
+		      ((<= 240 h 300) (list x 0 c))
+		      ((<= 300 h 360) (list c 0 x)))))
+    (mapcar (lambda (x) (+ x m)) rgb-n)))
+
+;;;
+;;; lists
+;;;
+
+(defun flatten (l &key (test #'atom))
+  (cond ((null l) nil)
+        ((funcall test l) (list l))
+        (t (loop for a in l nconc (flatten a :test test)))))
 
 ;;;
 ;;; loading-utils
@@ -20,28 +51,25 @@
 
 (defun make-text (string)
   (or (gethash string *text-hash*)
-      (progn
-	(when (> (hash-table-count *text-hash*) 64)
-	  (clean-text-hash))
-       (let* ((font         (sdl2-ttf:open-font
-			     (path "saxmono.ttf") 30))
-	      (texture      (car (gl:gen-textures 1)))
-	      (surface      (sdl2-ttf:render-utf8-blended
-			     font string 255 255 255 0))
-	      (surface-w    (surface-width surface))
-	      (surface-h    (surface-height surface))
-	      (surface-data (surface-pixels surface)))
-	 (gl:bind-texture  :texture-2d texture)
-	 (gl:tex-parameter :texture-2d :texture-min-filter :linear)
-	 (gl:tex-parameter :texture-2d :texture-mag-filter :linear)
-	 (gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
-			   :rgba :unsigned-byte surface-data)
-	 (free-surface surface)
-	 (sdl2-ttf:close-font font)
-	 (setf (gethash string *text-hash*)
-	       (make-text-texture :texture texture
-				  :width (/ surface-w 30)
-				  :heigth (/ surface-h 30 -1)))))))
+      (let* ((font         (sdl2-ttf:open-font
+			    (path "saxmono.ttf") 30))
+	     (texture      (car (gl:gen-textures 1)))
+	     (surface      (sdl2-ttf:render-utf8-blended
+			    font string 255 255 255 0))
+	     (surface-w    (surface-width surface))
+	     (surface-h    (surface-height surface))
+	     (surface-data (surface-pixels surface)))
+	(gl:bind-texture  :texture-2d texture)
+	(gl:tex-parameter :texture-2d :texture-min-filter :linear)
+	(gl:tex-parameter :texture-2d :texture-mag-filter :linear)
+	(gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
+			  :rgba :unsigned-byte surface-data)
+	(free-surface surface)
+	(sdl2-ttf:close-font font)
+	(setf (gethash string *text-hash*)
+	      (make-text-texture :texture texture
+				 :width (/ surface-w 30)
+				 :heigth (/ surface-h 30 -1))))))
 
 
 (defparameter *texture-hash* (make-hash-table :test #'equal))
@@ -53,66 +81,82 @@
   (clrhash *texture-hash*))
 
 (defun load-texture (file-name &optional (format :rgba)
-				         (filter :linear))
+				 (filter :linear))
   (or (gethash file-name *texture-hash*)
-      (progn
-	(when (> (hash-table-count *texture-hash*) 64)
-	  (clean-texture-hash))
-       (let* ((texture      (car (gl:gen-textures 1)))
-	      (surface      (sdl2-image:load-image (path file-name)))
-	      (surface-w    (surface-width surface))
-	      (surface-h    (surface-height surface))
-	      (surface-data (surface-pixels surface)))
-	 (gl:bind-texture  :texture-2d texture)
-	 (gl:tex-parameter :texture-2d :texture-min-filter filter)
-	 (gl:tex-parameter :texture-2d :texture-mag-filter filter)
-	 (gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
-			   format ; :bgr - bmp  :rgba - png
-			   :unsigned-byte surface-data)
-	 (free-surface surface)
-	 (setf (gethash file-name *texture-hash*) texture)))))
+      (let* ((texture      (car (gl:gen-textures 1)))
+	     (surface      (sdl2-image:load-image (path file-name)))
+	     (surface-w    (surface-width surface))
+	     (surface-h    (surface-height surface))
+	     (surface-data (surface-pixels surface)))
+	(gl:bind-texture  :texture-2d texture)
+	(gl:tex-parameter :texture-2d :texture-min-filter filter)
+	(gl:tex-parameter :texture-2d :texture-mag-filter filter)
+	(gl:tex-image-2d  :texture-2d 0 :rgba surface-w surface-h 0
+			  format ; :bgr - bmp  :rgba - png
+			  :unsigned-byte surface-data)
+	(free-surface surface)
+	(setf (gethash file-name *texture-hash*) texture))))
 
 ;;;
 ;;; drawing
 ;;;
 
 (defmacro define-textured (fname &body body)
-  `(defun ,fname (texture x y rotation
-		  scale-x &optional scale-y)
+  `(defun ,fname (texture x y rotation scale-x scale-y)
      (gl:bind-texture :texture-2d texture)
      (gl:push-matrix)
      (gl:translate x y 0)
      (gl:rotate rotation 0 0 1)
-     (gl:scale scale-x (or scale-y scale-x) 1)
+     (gl:scale scale-x scale-y 1)
      ,@body
      (gl:end)
      (gl:pop-matrix)))
 
 (defmacro define-shape (fname &body body)
-  `(defun ,fname (x y rotation
-		  scale-x &optional scale-y)
+  `(defun ,fname (x y rotation scale-x scale-y)
      (gl:bind-texture :texture-2d 0)
      (gl:push-matrix)
      (gl:translate x y 0)
      (gl:rotate rotation 0 0 1)
-     (gl:scale scale-x (or scale-y scale-x) 1)
+     (gl:scale scale-x scale-y 1)
      ,@body
      (gl:end)
      (gl:pop-matrix)))
 
 (define-textured quad-textured
   (gl:begin :triangle-fan)
-  (gl:tex-coord 0.0 0.0) (gl:vertex -0.1 -0.1)
-  (gl:tex-coord 1.0 0.0) (gl:vertex  0.1 -0.1)
-  (gl:tex-coord 1.0 1.0) (gl:vertex  0.1  0.1)
-  (gl:tex-coord 0.0 1.0) (gl:vertex -0.1  0.1))
+  (gl:tex-coord 0 0) (gl:vertex -1 -1)
+  (gl:tex-coord 1 0) (gl:vertex  1 -1)
+  (gl:tex-coord 1 1) (gl:vertex  1  1)
+  (gl:tex-coord 0 1) (gl:vertex -1  1))
 
 (define-shape quad-shape
   (gl:begin :triangle-fan)
-  (gl:tex-coord 0.0 0.0) (gl:vertex -0.1 -0.1)
-  (gl:tex-coord 1.0 0.0) (gl:vertex  0.1 -0.1)
-  (gl:tex-coord 1.0 1.0) (gl:vertex  0.1  0.1)
-  (gl:tex-coord 0.0 1.0) (gl:vertex -0.1  0.1))
+  (gl:vertex -1 -1)
+  (gl:vertex  1 -1)
+  (gl:vertex  1  1)
+  (gl:vertex -1  1))
+
+(define-shape aligned-quad-shape
+  (gl:begin :triangle-fan)
+  (gl:vertex  0  0)
+  (gl:vertex  1  0)
+  (gl:vertex  1  1)
+  (gl:vertex  0  1))
+
+(define-shape quad-lines
+  (gl:begin :line-loop)
+  (gl:vertex -1 -1)
+  (gl:vertex  1 -1)
+  (gl:vertex  1  1)
+  (gl:vertex -1  1))
+
+(define-shape aligned-quad-lines
+  (gl:begin :line-loop)
+  (gl:vertex  0  0)
+  (gl:vertex  1  0)
+  (gl:vertex  1  1)
+  (gl:vertex  0  1))
 
 (defun simple-line (x1 y1 x2 y2)
   (gl:bind-texture :texture-2d 0)
@@ -121,11 +165,24 @@
   (gl:vertex x2 y2)
   (gl:end))
 
+(defun simple-cross (x y size)
+  (simple-line (- x size) y
+	       (+ x size) y)
+  (simple-line x (- y size)
+	       x (+ y size)))
+
 (defun text (string x y size rotation)
   (let ((text-texture (make-text string)))
     (with-slots (texture width heigth) text-texture
       (quad-textured texture x y
 		     rotation (* width size) (* heigth size)))))
+
+(defun text-align (string x y size rotation)
+  (let ((text-texture (make-text string)))
+    (with-slots (texture width heigth) text-texture
+      (quad-textured texture (+ x (* width size)) y
+		     rotation (* width size) (* heigth size)))))
+
 
 ;;;
 ;;; window-utils
@@ -133,7 +190,9 @@
 
 (defun idler (window)
   "Every frame routines"
-  (let ((old-time (get-internal-real-time))
+  
+  (let ((old-time (get-internal-real-time)))
+	#|
 	(errors (nth-value
 		 1
 		 (ignore-errors ; in-game error messager
@@ -141,10 +200,13 @@
 		   
     (when errors
       (gl:load-identity)
-      (gl:color 1 1 1 1)
-      (text (make-text
-	     (write-to-string errors))
-	    0 0 0.4 0))
+      (gl:color 1 1 0)
+      (text "VLE ERROR"
+	    0 -0.8 0.03 0)
+      (text (write-to-string errors)
+	    0 -0.9 0.02 0)) |#
+
+    (main-screen *delta*)
     
     (gl:flush)
     (gl-swap-window window)
@@ -159,19 +221,19 @@
   (gl:matrix-mode :projection)
   (gl:load-identity)
   (let ((asp (/ width height)))
-    (gl:frustum (- asp) asp -1 1 1 100))
+    (gl:ortho (- asp) asp -1 1 0 1))
   (gl:translate 0 0 -1)
   (gl:matrix-mode :modelview))
 
-(defun full-screen (window)
-  (multiple-value-bind (some width height)
-      (get-current-display-mode 0)
-    (set-window-size window width height))
-  (set-window-fullscreen
-   window
-   (setf *fullscreen*
-	 (not *fullscreen*)))
-  (if *fullscreen* (hide-cursor) (show-cursor)))
+(let ((fullscreen nil))
+  (defun full-screen (window)
+    (multiple-value-bind (some width height)
+	(get-current-display-mode 0)
+      (set-window-size window width height))
+    (set-window-fullscreen
+     window
+     (setf fullscreen
+	   (not fullscreen)))))
 
 ;;;
 ;;; exebutable
